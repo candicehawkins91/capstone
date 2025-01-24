a2b09abfc5a66e10c6e7922649ba0163d6e74e77
candicehawkins91 <candicehawkins91@gmail.com>
2025-01-24 17:50:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strick";
+
+let week2Button = document.getElementById("week2");
+week2Button.addEventListener("click", () => {
+  window.location =
+    "https://codexacademy.moodlecloud.com/course/section.php?id=350";
+});
+
+let week3Button = document.getElementById("week3");
+week3Button.addEventListener("click", () => {
+  window.location =
+    "https://codexacademy.moodlecloud.com/course/section.php?id=351";
+});
+
+let week5Button = document.getElementById("week5");
+week5Button.addEventListener("click", () => {
+  window.location =
+    "https://codexacademy.moodlecloud.com/course/section.php?id=353";
+});
+
+let week6Button = document.getElementById("week6");
+week6Button.addEventListener("click", () => {
+  window.location =
+    "https://codexacademy.moodlecloud.com/course/section.php?id=354";
+});
+
+if ((buttonAddOn2 = document.getElementById("buttonAddOn2")));
+buttonAddOn2.addEventListener("click", () => {
+  alert: "We will contact you soon";
+});
